de690d0b019ae4044bb1e201dc4e849259d6b752
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2024-12-08 11:42:52 +0600

    handel the loader after reload

--- src/privetRoute/PrivateRoute.jsx
+++ src/privetRoute/PrivateRoute.jsx
@@ -3,9 +3,15 @@ import { Route, Navigate } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider'; 
 
 const PrivateRoute = ({ children }) => {
-    const { user } = useContext(AuthContext); 
+    const { user,loader } = useContext(AuthContext); 
 
-    
+    if (loader) {
+        return (
+          <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>
+        );
+      }
     return user ? children : <Navigate to="/login" />;
 };
 
